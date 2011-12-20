31cdff780743a781a390d241e92bdc0881177a13
Mario Scheliga <mario@sourcegarden.de>
2011-12-20 17:24:18 +0100

    push um zu pushen :D

--- liste.js
+++ liste.js
@@ -36,58 +36,67 @@ function WheelAccordionView(contentDomID, scrollDomID, topics) {
   self.updateContainerCSS = function () {
     console.log("udpate");
     var $container = $('#' + self.contentDomID),
-      containerCSS = 'height:' + $(window).height() + 'px; width:' + $(window).width() + 'px; border: 1px solid #FF0000; overflow:hidden; z-index:3; position:fixed; left: 0px; top: 0px;';
+      containerCSS = 'height:' + ($(window).height()-200) + 'px; width:' + $(window).width() + 'px; overflow:hidden; z-index:3; position:fixed; left: 0px; top: 200px;';
     $container.attr('style', containerCSS);
     self.windowHeight = $(window).height();
   };
 
   self.onScroll = function (event) {
     
-    var maxY = 5000 - self.windowHeight,
+    var maxY = 15000 - self.windowHeight,
       currentY = window.pageYOffset;
 
     var calc = function (itemNum, maxItems, maxHeight, currentY) {
       var lowerBoundary,
         itemStartY,
         currentHeight,
+        currentHeightBefore,
         currentPosY,
         defaultSpacing,
         query;
 
-      defaultSpacing = 10;
+      defaultSpacing = 20;
       lowerBoundary = (((itemNum - 1) / maxItems) * maxHeight);
       itemStartY = ((itemNum / maxItems) * maxHeight);
-      currentHeight = 400;
+      
       currentPosY = 0;
       query = "#" + self.contentDomID + " > div:nth-child(" + itemNum + ")";
+      queryBefore = "#" + self.contentDomID + " > div:nth-child(" + (itemNum - 1) + ")";
+      currentHeight = $(query + ' > div').height();
+      currentHeightBefore = $(queryBefore + ' > div').height();
       //console.log(query);
       var y = 0;
+      var yoffset = 0;
+      var deg = (itemNum % 2 === 0)? ('rotateZ(-2deg)'): ('rotateZ(2deg)');
       if (currentY > lowerBoundary && currentY < itemStartY) {
         // active
-        y = (itemNum * defaultSpacing);
+        y = (itemNum * defaultSpacing) + yoffset;
         //console.log("ITEM " + itemNum);
         //console.log("ITEM " + itemNum + " ----------- ( " + lowerBoundary + "; " + itemStartY + "; " + currentY + ")");
         //$(query).addClass('active');
         //$(query).attr('style', 'transition-property: height; transition-duration: 1s; height: 400px; top: 0px; left: 0px; position: absolute; width: 100%;');
       } else if (currentY > (itemStartY)) {
         // above active
-        y = (itemNum * defaultSpacing);
+        y = (itemNum * defaultSpacing) + yoffset;
+        
 
         //console.log("ITEM " + itemNum + " is top out");
         //$(query).removeClass('active');
        // $(query).attr('style', 'transition-property: height; transition-duration: 1s; height: 100px; position: absolute; top: ' + (200 * itemNum) + 'px; left: 0px; width: 100%;');
       } else {
         // below active
-        y = (((itemNum) * defaultSpacing)) + 400;
+        y = (((itemNum) * defaultSpacing)) + currentHeightBefore + yoffset;
         if (y + (currentY - lowerBoundary) > 0) {
           y = y - (y + (currentY - lowerBoundary));
         }
+        deg = (itemNum % 2 === 0)? ('rotateZ(2deg)'): ('rotateZ(-2deg)');
 
         //$(query).removeClass('active');
       }
-      var translate = 'translate3d(0px, ' + y + 'px, 0px)';
+      console.log(currentHeightBefore);
+      var translate = 'translate3d(-40px, ' + y + 'px, 0px)';
       //console.log("ITEM " + itemNum + ": " + translate + ' curY:' + (currentY - lowerBoundary));
-      var deg = (itemNum % 2 === 0)? ('rotateZ(-2deg)'): ('rotateZ(2deg)');
+      
       $(query).attr('style', '-webkit-transform:' + translate + deg + ';');
     };
     
